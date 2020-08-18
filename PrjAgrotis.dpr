0f5bc67572f054d802403d804f0d0dd32ee31486
Alexandre <alehsilva@live.com>
2020-08-18 01:03:00 -0300

    ClienteDao, ClienteController, Cliente Model implementados

--- PrjAgrotis.dpr
+++ PrjAgrotis.dpr
@@ -2,8 +2,13 @@ program PrjAgrotis;
 
 uses
   Vcl.Forms,
-  FPrincipalView in 'View\FPrincipalView.pas' {Form1},
-  uConexao in 'Dao\uConexao.pas';
+  uFrmPrincipalView in 'View\uFrmPrincipalView.pas' {Form1},
+  uConexao in 'Dao\uConexao.pas',
+  uEnumerado in 'Model\uEnumerado.pas',
+  uClienteModel in 'Model\uClienteModel.pas' {$R *.res},
+  uFrmCliente in 'View\uFrmCliente.pas' {frmCadastroCliente},
+  uClienteDao in 'Dao\uClienteDao.pas',
+  uClienteControl in 'Controller\uClienteControl.pas';
 
 {$R *.res}
 
